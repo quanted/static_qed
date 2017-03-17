36f34b696ad169554e27eae2309fffb2c0ec9917
dbsmith88 <dbsmith@uga.edu>
2017-03-17 17:10:57 -0400

    js updates and new js files added

--- hms/js/scripts_hms_inputs.js
+++ hms/js/scripts_hms_inputs.js
@@ -10,8 +10,25 @@ $(document).ready(function () {
     //$("#id_orchard_type option[value='Dense']").prop('disabled',true);
     //$("#id_orchard_type option[value='Sparse']").prop('disabled',true);
 
-    //$('#id_application_method').change(function () {
-    //    if ($(this).val() == "Tier I Ground") {
+        $('#id_source').change(function () {
+
+            if ($(this).val() == "NLDAS" && $('#submodel') == "soilmoisture") {
+                $('#id_layers').clean();
+                $('#id_layers').append($("<option>").val(0).html("0-10cm"));
+                $('#id_layers').append($("<option>").val(1).html("10-40cm"));
+                $('#id_layers').append($("<option>").val(2).html("40-100cm"));
+                $('#id_layers').append($("<option>").val(3).html("100-200cm"));
+                $('#id_layers').append($("<option>").val(4).html("0-100cm"));
+                $('#id_layers').append($("<option>").val(5).html("0-200cm"));
+            }
+            else if($(this).val() == "GLDAS" && $('#submodel') == "soilmoisture"){
+                $('#id_layers').clean();
+                $('#id_layers').append($("<option>").val(0).html("0-10cm"));
+                $('#id_layers').append($("<option>").val(1).html("10-40cm"));
+                $('#id_layers').append($("<option>").val(2).html("40-100cm"));
+                $('#id_layers').append($("<option>").val(3).html("0-100cm"));
+            }
+        });
     //        $('#id_boom_height').closest('tr').show();
     //        $('#id_orchard_type').closest('tr').hide();
             //$('#id_drop_size').closest('tr').show();
