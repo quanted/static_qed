e918f4d2e8466af1789a243a48354bc955d3d7b7
dbsmith88 <dbsmith@uga.edu>
2017-08-17 12:11:02 -0400

    update for soilmoisture options

--- hms/js/scripts_hms_inputs.js
+++ hms/js/scripts_hms_inputs.js
@@ -9,19 +9,19 @@ $(document).ready(function() {
             var element = document.getElementById("id_source");
             if(element.options[element.selectedIndex].value === 'NLDAS'){
                  document.input_table.layers.options.length = 0;
-                 document.input_table.layers.options[0] = new Option("0-10cm", 0);
-                 document.input_table.layers.options[1] = new Option("10-40cm", 1);
-                 document.input_table.layers.options[2] = new Option("40-100cm", 2);
-                 document.input_table.layers.options[3] = new Option("100-200cm", 3);
-                 document.input_table.layers.options[4] = new Option("0-100cm", 4);
-                 document.input_table.layers.options[5] = new Option("0-200cm", 5);
+                 document.input_table.layers.options[0] = new Option("0-10cm", "0-10");
+                 document.input_table.layers.options[1] = new Option("10-40cm", "10-40");
+                 document.input_table.layers.options[2] = new Option("40-100cm", "40-100");
+                 document.input_table.layers.options[3] = new Option("100-200cm", "100-200");
+                 document.input_table.layers.options[4] = new Option("0-100cm", "0-100");
+                 document.input_table.layers.options[5] = new Option("0-200cm", "0-200");
              }
              else if (element.options[element.selectedIndex].value === 'GLDAS') {
                 document.input_table.layers.options.length = 0;
-                document.input_table.layers.options[0] = new Option("0-10cm", 0);
-                document.input_table.layers.options[1] = new Option("10-40cm", 1);
-                document.input_table.layers.options[2] = new Option("40-100cm", 2);
-                document.input_table.layers.options[3] = new Option("0-100cm", 3);
+                document.input_table.layers.options[0] = new Option("0-10cm", "0-10");
+                document.input_table.layers.options[1] = new Option("10-40cm", "10-40");
+                document.input_table.layers.options[2] = new Option("40-100cm", "40-100");
+                document.input_table.layers.options[3] = new Option("0-100cm", "0-100");
              }
         }
     });
